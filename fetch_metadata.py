f98b58b1e5befb90025c88edd3dde98128ac1d18
throwaway1245725 <htea@htea.htea>
2023-10-06 15:27:29 -0400

    added support for local metadata

--- fetch_metadata.py
+++ fetch_metadata.py
@@ -1,13 +1,17 @@
 import collections
 import json
 import re
+from datetime import datetime
 from pathlib import Path
 from time import sleep
 from typing import Dict
 
 import requests
+import yaml
 from bs4 import BeautifulSoup
 from tinydb import TinyDB
+
+KSK_DED = True
 
 PLURAL_MAP = {
     "artist": "artists",
@@ -19,6 +23,8 @@ PLURAL_MAP = {
 PAGES_PATTERN = re.compile(r"\/browse\?ps=(?P<pages>\d+)&adv=")
 HREF_PATTERN = re.compile(r"\/.+\/(?P<name>.+)")
 
+IMAGE_SUFFIXES = [".jpg", ".jpeg", ".png"]
+
 index_json = Path.cwd() / "index.json"
 data_dir = Path.cwd() / "data"
 db = TinyDB("db.json", indent=2, ensure_ascii=False, sort_keys=True, encoding="utf-8")
@@ -27,6 +33,77 @@ db = TinyDB("db.json", indent=2, ensure_ascii=False, sort_keys=True, encoding="u
 def resolve_external_url(ksk_url: str) -> str:
     response = requests.get(f"https://ksk.moe{ksk_url}")
     return response.url
+
+
+def get_local_metadata(entry_path: Path):
+    yaml_file = next(file for file in entry_path.iterdir() if file.suffix == ".yaml")
+    print(f"found metadata file: {yaml_file}")
+    with yaml_file.open("r") as f:
+        yaml_data = yaml.safe_load(f)
+
+    metadata = {
+        "title": None,
+        "archive_name": None,
+        "category": None,
+        "uploader": None,
+        "artists": [],
+        "circles": None,
+        "magazines": None,
+        "parodies": None,
+        "tags": [],
+        "pages": 0,
+        "official_sources": None,
+        "size": None,
+        "date_uploaded": None,
+        "date_archived": None,
+        "date_published": None,
+        "date_updated": None,
+        "description": None,
+    }
+
+    metadata["title"] = yaml_data["Title"]
+
+    metadata["artists"] = yaml_data.get("Artist", [])
+    metadata["circles"] = yaml_data.get("Circle", None)
+    artists_str = ",".join(metadata["artists"])
+    circles_str = ",".join(metadata["circles"]) if metadata["circles"] else ""
+    metadata[
+        "archive_name"
+    ] = f"[{f'{circles_str}[{artists_str}]' if circles_str else artists_str}] {entry_path.name}"
+
+    metadata["category"] = "Manga" if "Magazine" in yaml_data.keys() else "Doujinshi"
+    metadata["magazines"] = yaml_data.get("Magazine", None)
+    metadata["parodies"] = yaml_data.get("Parody", None)
+    metadata["tags"] = yaml_data["Tags"]
+    metadata["pages"] = yaml_data["Pages"]
+    metadata["official_sources"] = {
+        "display_name": ",".join(yaml_data.get("Publisher", ["FAKKU"])),
+        "url": yaml_data["URL"],
+    }
+
+    size_in_bytes = sum(
+        image.stat().st_size
+        for image in entry_path.iterdir()
+        if image.suffix in IMAGE_SUFFIXES
+    )
+    metadata[
+        "size"
+    ] = f"{round(size_in_bytes / ((2**10)**2))} MiB ({size_in_bytes:,} bytes)"
+
+    epoch = yaml_data.get("Released", None)
+    if epoch:
+        date_dict = {
+            "display": datetime.fromtimestamp(epoch).strftime("%d.%m.%Y %H:%M UTC"),
+            "epoch": epoch,
+        }
+        metadata["date_archived"] = date_dict
+        metadata["date_published"] = date_dict
+        metadata["date_updated"] = date_dict
+        metadata["date_uploaded"] = date_dict
+
+    metadata["description"] = yaml_data.get("Description", None)
+
+    return metadata
 
 
 def get_metadata(url):
@@ -166,28 +243,43 @@ def get_missing_metadata():
     for artist, entries in index_data.items():
         for entry, url in entries.items():
             metadata_json = data_dir / artist / entry / "metadata.json"
+
             if not metadata_json.exists():
-                print(f"fetching metadata for {artist}/{entry}")
                 try:
+                    if KSK_DED:
+                        raise Exception("ksk ded")
+                    print(f"fetching metadata for {artist}/{entry}")
                     metadata = get_metadata(url)
-                    with metadata_json.open(mode="w", encoding="utf-8") as f:
-                        json.dump(
-                            obj=metadata,
-                            fp=f,
-                            indent=2,
-                            ensure_ascii=False,
-                            sort_keys=True,
-                        )
-                        f.write("\n")
-                    db.insert(
-                        {
-                            **metadata,
-                            "json_path": metadata_json.relative_to(data_dir).as_posix(),
-                        }
-                    )
+                    write_metadata(metadata_json, metadata)
                 except Exception as e:
-                    print(f"failed to fetch metadata for {url}")
-                    print(e)
+                    if not KSK_DED:
+                        print(f"failed to fetch metadata for {url}")
+                        print(e)
+                    try:
+                        print(f"fetching local metadata for {artist}/{entry}")
+                        metadata = get_local_metadata(data_dir / artist / entry)
+                        write_metadata(metadata_json, metadata)
+                    except Exception as e:
+                        print(f"failed to fetch local metadata for {artist}/{entry}")
+                        print(e)
+
+
+def write_metadata(metadata_json, metadata):
+    with metadata_json.open(mode="w", encoding="utf-8") as f:
+        json.dump(
+            obj=metadata,
+            fp=f,
+            indent=2,
+            ensure_ascii=False,
+            sort_keys=True,
+        )
+        f.write("\n")
+    db.insert(
+        {
+            **metadata,
+            "json_path": metadata_json.relative_to(data_dir).as_posix(),
+        }
+    )
 
 
 get_missing_metadata()
--- search_metadata.py
+++ search_metadata.py
@@ -6,14 +6,30 @@ from tinydb import Query, TinyDB
 db = TinyDB("db.json", indent=2, ensure_ascii=False, sort_keys=True, encoding="utf-8")
 data_dir = Path.cwd() / "data"
 
+# https://tinydb.readthedocs.io/en/latest/usage.html
 
-def fetch_metadata(json_path: Path) -> dict:
-    with json_path.open(encoding="utf-8") as f:
-        return {**json.load(f), "json_path": json_path.relative_to(data_dir).as_posix()}
+#
+Entry = Query()
+# db.search(Entry.title == "")
+# db.search(Entry.title.matches("[aZ]*"))
+# db.search(Entry.artists.any[""])
+# db.search(Entry.tags.any["", ""])
 
 
-all_metadata_files = list(data_dir.glob("**/metadata.json"))
-if len(db) != len(all_metadata_files):
-    db.drop_tables()
-    db.insert_multiple(map(fetch_metadata, all_metadata_files))
+def check_db_file():
+    def fetch_metadata(json_path: Path) -> dict:
+        with json_path.open(encoding="utf-8") as f:
+            return {
+                **json.load(f),
+                "json_path": json_path.relative_to(data_dir).as_posix(),
+            }
+
+    all_metadata_files = list(data_dir.glob("**/metadata.json"))
+    if len(db) != len(all_metadata_files):
+        db.drop_tables()
+        db.insert_multiple(map(fetch_metadata, all_metadata_files))
+
+
 print("oh")
+
+# check_db_file()
