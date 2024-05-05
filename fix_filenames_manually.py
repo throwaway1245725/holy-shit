ea2a9d352d6355193ff93f85824901fc1d17d02f
throwaway1245725 <htea@htea.htea>
2024-05-05 12:09:21 -0400

    daily, replaced stuff too

--- fix_filenames_manually.py
+++ fix_filenames_manually.py
@@ -5,5 +5,5 @@ p = re.compile(r".*p(\d{3}) \[x3200\].*")
 for i in Path.cwd().iterdir():
     m = p.match(i.stem)
     if m:
-        # print(i.with_name(f"{m.group(1)}{i.suffix}"))
-        i.rename(i.with_name(f"{m.group(1)}{i.suffix}"))
+        print(i.with_name(f"{int(m.group(1)):02}{i.suffix}"))
+        # i.rename(i.with_name(f"{int(m.group(1)):02}{i.suffix}"))
