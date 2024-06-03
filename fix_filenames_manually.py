e339e01f9af91ff3bd97e1082521eba934ca19e1
throwaway1245725 <htea@htea.htea>
2024-06-03 13:10:57 -0400

    there is hope

--- fix_filenames_manually.py
+++ fix_filenames_manually.py
@@ -1,9 +1,9 @@
 import re
 from pathlib import Path
 
-p = re.compile(r".*p(\d{3}) \[x3200\].*")
+p = re.compile(r"[^\d]+(\d+)")
 for i in Path.cwd().iterdir():
     m = p.match(i.stem)
     if m:
-        print(i.with_name(f"{int(m.group(1)):02}{i.suffix}"))
-        # i.rename(i.with_name(f"{int(m.group(1)):02}{i.suffix}"))
+        print(i.with_name(f"{int(m.group(1))+1:02}{i.suffix}"))
+        # i.rename(i.with_name(f"{int(m.group(1))+1:02}{i.suffix}"))
