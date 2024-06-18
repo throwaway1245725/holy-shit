828aec41f312f55cabd4ae52a986396697616eae
throwaway1245725 <htea@htea.htea>
2024-06-18 04:16:57 -0400

    big update, hn added missing stuff

--- fix_filenames_manually.py
+++ fix_filenames_manually.py
@@ -1,9 +1,9 @@
 import re
 from pathlib import Path
 
-p = re.compile(r"[^\d]+(\d+)")
+p = re.compile(r".*p(\d+).*")
 for i in Path.cwd().iterdir():
     m = p.match(i.stem)
     if m:
-        print(i.with_name(f"{int(m.group(1))+1:02}{i.suffix}"))
-        # i.rename(i.with_name(f"{int(m.group(1))+1:02}{i.suffix}"))
+        print(i.with_name(f"{int(m.group(1)):02}{i.suffix}"))
+        # i.rename(i.with_name(f"{int(m.group(1)):02}{i.suffix}"))
