172532f41d642e47dc4f79aa981db4301c69b1e3
throwaway1245725 <htea@htea.htea>
2024-06-18 04:16:38 -0400

    added new downloader for hn

---
+++ download_favorited.py
@@ -0,0 +1,159 @@
+import json
+import os
+import re
+import shutil
+from email.message import Message
+from pathlib import Path
+from time import sleep
+from typing import Dict
+
+import requests
+from tqdm import tqdm
+
+from log_setup import log
+
+HN_BASE_URL = os.getenv("HN_BASE_URL", "")
+HN_ID_PATTERN = re.compile(f"{re.escape(HN_BASE_URL)}/view/(\\d+)")
+
+
+download_dir = Path.cwd() / "downloaded"
+favorited_json = Path.cwd() / "favorited.json"
+downloaded_json = Path.cwd() / "downloaded.json"
+
+with favorited_json.open("r", encoding="utf-8") as f:
+    favorited_data: Dict[str, str] = json.load(f)
+
+cookies_txt = Path.cwd() / "hn_cookies.txt"
+with cookies_txt.open("r") as f:
+    cookies_str = f.read()
+    cookies_dict = {
+        cookie.split("=")[0]: cookie.split("=")[1] for cookie in cookies_str.split("; ")
+    }
+
+
+def clean_download_index():
+    downloaded_archives = set(archive.stem for archive in download_dir.iterdir())
+    with downloaded_json.open("r+", encoding="utf-8") as f:
+        downloaded_data: Dict[str, str] = json.load(f)
+        downloaded_archives_index = set(downloaded_data.values())
+        extra_archives_str = "\n".join(downloaded_archives_index - downloaded_archives)
+        if extra_archives_str:
+            log.warning(
+                f"difference between actually downloaded and indexed downloaded: \n{extra_archives_str}"
+            )
+        downloaded_data = {
+            url: archive_filename
+            for url, archive_filename in downloaded_data.items()
+            if archive_filename in downloaded_archives
+        }
+        downloaded_data = dict(
+            sorted(downloaded_data.items(), key=lambda item: item[1])
+        )
+        f.seek(0)
+        json.dump(obj=downloaded_data, fp=f, indent=2, ensure_ascii=False)
+        f.write("\n")
+        f.truncate()
+
+
+def download_all_favorites():
+    for url, path in favorited_data.items():
+        with downloaded_json.open("r", encoding="utf-8") as f:
+            downloaded_data: Dict[str, str] = json.load(f)
+        if not url in downloaded_data.keys():
+            log.warning(f"downloading favorite: '{url} : {path}'")
+            download_archive(url)
+
+
+def extract_filename(headers):
+    msg = Message()
+    msg["content-disposition"] = headers["content-disposition"]
+    filename = msg.get_filename()
+    if filename:
+        return re.sub(r"[<>:\"/\\|?*]", "_", filename)
+    else:
+        raise Exception("not supposed to happen!")
+
+
+def download_file(filepath: Path, response: requests.Response):
+    if filepath.exists():
+        raise Exception("file already exists?!")
+    file_size = int(response.headers["content-length"])
+    chunk_size = 1024 * 16
+    with open(filepath, "wb") as f:
+        with tqdm(
+            total=file_size,
+            desc=f"Downloading {filepath.name}",
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+            miniters=1,
+        ) as bar:
+            for chunk in response.iter_content(chunk_size=chunk_size):
+                f.write(chunk)
+                bar.update(chunk_size)
+
+
+def download_archive(url):
+    if m := HN_ID_PATTERN.match(url):
+        dl_url = f"{HN_BASE_URL}/zip/{m.group(1)}"
+        with requests.get(dl_url, stream=True, cookies=cookies_dict) as r:
+            filepath = download_dir / extract_filename(r.headers)
+            download_file(filepath, r)
+            log.info(f"finished downloading '{url} : {filepath}'")
+            if filepath.suffix != ".cbz":
+                filepath_cbz = filepath.with_suffix(".cbz")
+                filepath.rename(filepath_cbz)
+                filepath = filepath_cbz
+                log.info(f"renamed zip to cbz: {filepath}")
+            write_to_downloaded_json(url, filepath.stem)
+
+
+def write_to_downloaded_json(url: str, filename: str):
+    with downloaded_json.open("r+", encoding="utf-8") as f:
+        downloaded_data: Dict[str, str] = json.load(f)
+        if url not in downloaded_data.keys():
+            downloaded_data[url] = filename
+            downloaded_data = dict(
+                sorted(downloaded_data.items(), key=lambda item: item[1])
+            )
+            f.seek(0)
+            json.dump(obj=downloaded_data, fp=f, indent=2, ensure_ascii=False)
+            f.write("\n")
+            f.truncate()
+
+
+clean_download_index()
+download_all_favorites()
+
+# a_index_archive_json = Path.cwd() / "legacy" / "a_index_archive.json"
+# index_json = Path.cwd() / "index.json"
+
+# with a_index_archive_json.open("r", encoding="utf-8") as f:
+#     a_index_archive_data: Dict[str, Dict[str, str]] = json.load(f)
+
+# with index_json.open("r", encoding="utf-8") as f:
+#     index_data: Dict[str, Dict[str, str]] = json.load(f)
+
+
+# def fix_downloaded_urls():
+#     a_to_hn = {
+#         a_url: index_data[artist][path]
+#         for artist, entries in a_index_archive_data.items()
+#         for path, a_url in entries.items()
+#     }
+#     with downloaded_json.open("r+", encoding="utf-8") as f:
+#         downloaded_data: Dict[str, str] = json.load(f)
+#         downloaded_data = {
+#             a_to_hn[url] if url in a_to_hn else url: archive_filename
+#             for url, archive_filename in downloaded_data.items()
+#         }
+#         downloaded_data = dict(
+#             sorted(downloaded_data.items(), key=lambda item: item[1])
+#         )
+#         f.seek(0)
+#         json.dump(obj=downloaded_data, fp=f, indent=2, ensure_ascii=False)
+#         f.write("\n")
+#         f.truncate()
+
+
+# fix_downloaded_urls()
