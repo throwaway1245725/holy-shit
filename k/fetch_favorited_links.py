1d0f371ee36eae9a9db630186850c10c81f7331a
throwaway1245725 <htea@htea.htea>
2025-07-24 11:30:58 -0400

    hn

--- k/fetch_favorited_links.py
+++ k/fetch_favorited_links.py
@@ -38,7 +38,7 @@ def get_url(url: str, params=None) -> requests.Response:
             "Authorization": f"Bearer {localstorage_dict["token"]["session"]}",
             "Origin": K_BASE_URL,
             "Referer": f"{K_BASE_URL}/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
         },
     )
 
